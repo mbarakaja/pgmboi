f0b084a1ea1942753b7a819f0c75c8678734b361
mbarakaja <miso@kojoi.com>
2015-07-29 13:50:19 -0400

    new(__init__): add a new gobal `config` instance

--- pgmboi/__init__.py
+++ pgmboi/__init__.py
@@ -4,3 +4,20 @@ __version__ = '0.0.1'
 
 # The directory where the application was called
 working_dir = getcwdu()
+
+from config import Config
+config = Config()
+
+
+def parse_dir(*args, **kwargs):
+    '''Generates  a directory path string'''
+
+    slash = '/'
+    _path = working_dir
+    for arg in args:
+        _path += slash + arg
+
+    if 'ext' in kwargs:
+        _path += '.' + kwargs['ext']
+
+    return _path
