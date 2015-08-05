faf042a55c77b4c9121c1001cbfb56f5881f6464
mbarakaja <miso@kojoi.com>
2015-08-05 06:42:56 -0400

    feat(cli): add `dump` command and connection arguments to CLI app
    Added `dump` command to the CLI application for dump a database and
    aditionaly added --host, --port, --database, --user and --password
    arguments for database connection

--- pgmboi/cli.py
+++ pgmboi/cli.py
@@ -1,2 +1,54 @@
-def main():
-    print 'mojojoj'
+import sys
+import click
+from click import secho
+from .dump import dump_database
+from . import database, config
+
+
+@click.group()
+@click.option('--host', help='database server host')
+@click.option('--port', help='connection port number')
+@click.option('--database', help='the name of the database')
+@click.option('--user', help='user name')
+@click.option('--password', help='password')
+def main(host, port, database, user, password):
+    """Dump a PostgreSQL database schema for easy revision control."""
+
+    global config
+
+    # star trying to load the configuration from the file system
+    config.load_json()
+    print config
+
+    if host:
+        config.host = host
+
+    if port:
+        config.port = port
+
+    if database:
+        config.database = database
+
+    if user:
+        config.user = host
+
+    if password:
+        config.password = password
+
+    # Recheck if the configuration is valid or sufficient
+    # to run the application
+    if not config.is_valid:
+        secho('Wrong config, something is missing:', fg='red')
+        secho(str(config))
+        sys.exit(1)
+
+    return True
+
+
+@main.command()
+def dump():
+    if not database.connect(config):
+        sys.exit(1)
+
+    dump_database()
+    database.close()
