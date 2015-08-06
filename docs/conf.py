7e1891d64f3421bd19674d31d219f0a329154f30
mbarakaja <miso@kojoi.com>
2015-08-06 14:43:34 -0400

    chore(conf.py): remove `sphinx.ext.viewcode` extension

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,6 @@ import pkg_resources
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.todo',
-    'sphinx.ext.viewcode',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
